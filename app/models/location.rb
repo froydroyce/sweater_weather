144d98240453b6fe32bb38207cc6c15f5db259bf
Sejin Kim <froydroyce@gmail.com>
2019-09-15 21:17:37 -0600

    Create location model

---
+++ app/models/location.rb
@@ -0,0 +1,11 @@
+class Location
+  attr_reader :lat, :lng
+  
+  def initialize(attr)
+    @city = attr[:address_components][0][:long_name]
+    @state = attr[:address_components][2][:short_name]
+    @country = attr[:address_components][3][:long_name]
+    @lat = attr[:geometry][:location][:lat]
+    @lng = attr[:geometry][:location][:lng]
+  end
+end
