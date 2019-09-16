ad5a7fc6c6173303f0c4ce290ab5cef09e5aefdd
Sejin Kim <froydroyce@gmail.com>
2019-09-16 17:24:51 -0600

    Update spec to use correct image url

--- spec/requests/api/v1/background_request_spec.rb
+++ spec/requests/api/v1/background_request_spec.rb
@@ -3,15 +3,15 @@ require "rails_helper"
 describe 'Backgrounds API' do
   it "gets an image" do
     json_response = File.open("./fixtures/denver_flickr.json")
-    stub_request(:get, "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=#{ENV['FLICKR_API']}&text=denver,co,skyline&geo_context=2&per_page=1&accuracy=10&format=json").
+    stub_request(:get, "https://www.flickr.com/services/rest/?accuracy=10&api_key=#{ENV['FLICKR_API']}&format=json&geo_context=2&method=flickr.photos.search&nojsoncallback=1&per_page=1&text=denver,co,%20skyline").
       to_return(status: 200, body: json_response)
-      
+
     get '/api/v1/backgrounds?location=denver,co'
 
     expect(response).to be_successful
     image = JSON.parse(response.body)
 
     expect(image["data"].count).to eq(3)
-    expect(image["data"]["attributes"]["image"]["url"]).to eq("https://farm66.staticflickr.com/65535/48628059008_9ab1b6c1ac.jpg")
+    expect(image["data"]["attributes"]["image"]["url"]).to eq("https://farm1.staticflickr.com/930/43691964192_4ff507454d.jpg")
   end
 end
