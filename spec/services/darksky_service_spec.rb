82cd15383881b6a876e1e2734d96f85adaa56328
Sejin Kim <froydroyce@gmail.com>
2019-09-16 17:46:06 -0600

    Refactor attributes

--- spec/services/darksky_service_spec.rb
+++ spec/services/darksky_service_spec.rb
@@ -6,24 +6,10 @@ describe DarkskyService do
       dark_response = File.open("./fixtures/denver_darksky.json")
       stub_request(:get, "https://api.darksky.net/forecast/#{ENV['DARKSKY_API']}/39.7392358,-104.990251").
         to_return(status: 200, body: dark_response)
+      response = File.read("./fixtures/denver_geocode.json")
+      attr = JSON.parse(response, symbolize_names: true)
 
-      attr = {
-        :address_components=>
-          [{:long_name=>"Denver", :short_name=>"Denver", :types=>["locality", "political"]},
-            {:long_name=>"Denver County", :short_name=>"Denver County", :types=>["administrative_area_level_2", "political"]},
-            {:long_name=>"Colorado", :short_name=>"CO", :types=>["administrative_area_level_1", "political"]},
-            {:long_name=>"United States", :short_name=>"US", :types=>["country", "political"]}],
-          :formatted_address=>"Denver, CO, USA",
-          :geometry=>
-            {:bounds=>{:northeast=>{:lat=>39.91424689999999, :lng=>-104.6002959}, :southwest=>{:lat=>39.614431, :lng=>-105.109927}},
-              :location=>{:lat=>39.7392358, :lng=>-104.990251},
-              :location_type=>"APPROXIMATE",
-              :viewport=>{:northeast=>{:lat=>39.91424689999999, :lng=>-104.6002959}, :southwest=>{:lat=>39.614431, :lng=>-105.109927}}},
-          :place_id=>"ChIJzxcfI6qAa4cR1jaKJ_j0jhE",
-          :types=>["locality", "political"]
-        }
-
-      location = Location.new(attr)
+      location = Location.new(attr[:results][0])
       forecast = subject.forecast_by(location)
 
       expect(forecast).to be_an Hash
