0cbb07a69c1057498fb1ace4605dda8e5488af89
Sejin Kim <froydroyce@gmail.com>
2019-09-16 17:54:48 -0600

    Refactor stub

--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -4,26 +4,7 @@ describe Forecast do
   it "exists" do
     dark_response = File.read("./fixtures/denver_darksky.json")
     attr = JSON.parse(dark_response, symbolize_names: true)
-    allow_any_instance_of(Forecast).to receive(:tonight).and_return(
-      [{"time"=>1568599200,
-      "summary"=>"Mostly Cloudy",
-      "icon"=>"partly-cloudy-night",
-      "precipIntensity"=>0.0005,
-      "precipProbability"=>0.01,
-      "precipType"=>"rain",
-      "temperature"=>84.13,
-      "apparentTemperature"=>84.13,
-      "dewPoint"=>32.1,
-      "humidity"=>0.15,
-      "pressure"=>1009.77,
-      "windSpeed"=>8.14,
-      "windGust"=>14.16,
-      "windBearing"=>171,
-      "cloudCover"=>0.76,
-      "uvIndex"=>0,
-      "visibility"=>10,
-      "ozone"=>279.2}]
-    )
+    allow_any_instance_of(DateTime).to receive(:to_time).and_return(1568599200)
     forecast = Forecast.new(attr)
 
     expect(forecast).to be_a Forecast
