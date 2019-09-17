50c6f34b4e3dce841dd79778639e992f25dac607
Sejin Kim <froydroyce@gmail.com>
2019-09-17 14:00:31 -0600

    Update spec to test for sad paths

---
+++ spec/requests/api/v1/account_creation_request_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+describe "Acount Creation" do
+  it "returns an api_key" do
+    allow(SecureRandom)
+      .to receive(:urlsafe_base64)
+      .and_return("Kgs1XMbmJMzj-iVyz8ZWbg")
+
+    post "/api/v1/users?email=whatever@exeample.com&password=password&password_confirmation=password"
+
+    expect(response).to be_successful
+    api_key = JSON.parse(response.body)
+
+    expect(api_key["data"].count).to eq(3)
+    expect(api_key["data"]["attributes"]["api_key"]).to eq("Kgs1XMbmJMzj-iVyz8ZWbg")
+  end
+
+  it "does not return an api_key" do
+    post "/api/v1/users?email=&password=&password_confirmation="
+
+    expect(response).to_not be_successful
+    bad_request = JSON.parse(response.body)
+
+    expect(bad_request["data"]["attributes"]["errors"]).to eq("Password can't be blank and Email can't be blank")
+  end
+end
