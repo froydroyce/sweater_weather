cd691451e676137033ac2e24923f556c9d5ba30e
Sejin Kim <froydroyce@gmail.com>
2019-09-17 20:05:59 -0600

    Create road trip controller

---
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::RoadTripController < ApplicationController
+  def create
+    user = User.find_by(api_key: params[:api_key])
+    if user
+      render json: RoadTripSerializer.new(RoadTripFacade.new(params))
+    else
+      render json: "Invalid or no api_key", status: :unauthorized
+    end
+  end
+end
