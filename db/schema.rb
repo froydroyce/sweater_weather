3d75aabaeb0df6d0066dfec98459d142a59e74b8
Sejin Kim <froydroyce@gmail.com>
2019-09-17 23:55:39 -0600

    Convert at to use postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_09_17_184148) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
