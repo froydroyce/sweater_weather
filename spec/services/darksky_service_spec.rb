50192cd2b90de643b891ce58f35fcc2ff496c9ed
Sejin Kim <froydroyce@gmail.com>
2019-09-17 20:32:01 -0600

    Add test for future forecast

--- spec/services/darksky_service_spec.rb
+++ spec/services/darksky_service_spec.rb
@@ -22,5 +22,27 @@ describe DarkskyService do
       expect(forecast).to have_key(:hourly)
       expect(forecast).to have_key(:daily)
     end
+
+    it "returns future forecast data" do
+      dark_response = File.open("./fixtures/pueblo_darksky.json")
+      stub_request(:get, "https://api.darksky.net/forecast/#{ENV['DARKSKY_API']}/38.2544472,-104.6091409,1568581207").
+        to_return(status: 200, body: dark_response)
+      allow_any_instance_of(Time).to receive(:to_i).and_return(1568574824)
+
+      response = File.read("./fixtures/pueblo_geocode.json")
+      attr = JSON.parse(response, symbolize_names: true)
+
+      location = Location.new(attr[:results][0])
+      forecast = subject.future_forecast_by(location, 1568581207)
+
+      expect(forecast).to be_an Hash
+      expect(forecast.count).to eq(8)
+      expect(forecast).to have_key(:latitude)
+      expect(forecast).to have_key(:longitude)
+      expect(forecast).to have_key(:timezone)
+      expect(forecast).to have_key(:currently)
+      expect(forecast).to have_key(:hourly)
+      expect(forecast).to have_key(:daily)
+    end
   end
 end
