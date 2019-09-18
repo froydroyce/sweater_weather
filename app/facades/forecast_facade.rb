9217c4a57640b46166e224560d1119c3b24e30a0
Sejin Kim <froydroyce@gmail.com>
2019-09-17 20:24:48 -0600

    Create future_forecast method

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -13,6 +13,10 @@ class ForecastFacade
     Forecast.new(forecast_for(location))
   end
 
+  def future_forecast(arrival_time)
+    Forecast.new(future_forecast_for(location, arrival_time))
+  end
+
   private
 
   def geolocation(city_state)
@@ -21,6 +25,10 @@ class ForecastFacade
 
   def forecast_for(location)
     darksky_service.forecast_by(location)
+  end
+
+  def future_forecast_for(location, arrival_time)
+    darksky_service.future_forecast_by(location, arrival_time)
   end
 
   def geo_service
